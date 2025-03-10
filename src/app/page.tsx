d487782a332cf3637202c63046e8f0e1d96746dc
syarif <syarif.khalid@gmail.com>
2025-03-10 23:57:08 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Sidebar from "../components/sidebar"
 import Navbar from "../components/navbar"
+import { FaStar } from "react-icons/fa";
+import { IoIosStar } from "react-icons/io";
+import { IoIosStarHalf } from "react-icons/io";
 
 export default function Home() {
   return (
@@ -9,6 +12,29 @@ export default function Home() {
       </div>
       <div className="w-4/5">
         <Navbar/>
+        <div className="my-4 ml-4">
+          <span className="font-bold text-3xl">Dashboard</span>
+        </div>
+        <div className="flex">
+          <div className="bg-purple-400 flex flex-col p-4 rounded-lg">
+            <div className="flex items-center gap-2">
+              <div className="bg-purple-200 flex p-2 rounded-lg">
+                <FaStar/>
+              </div>
+              <span>NILAI RATA-RATA</span>
+            </div>
+            <div className="flex items-center justify-around">
+              <span>7,5</span>
+              <div className="text-yellow-500 flex items-center">
+                <IoIosStar/>
+                <IoIosStar/>
+                <IoIosStar/>
+                <IoIosStar/>
+                <IoIosStarHalf/>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
